295f3df7cafc13584938e1ebd1e58af6ff1c959a
akarsh <akarshagri121@gmail.com>
2023-03-30 15:21:47 +0530

    aks

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ app.use(express.json());
 
 app.post('/git-webhook', (req, res) => {
    console.log("Git init");
-   const { ref } = req.body;
-   if (ref === 'refs/heads/main' || ref === 'refs/heads/master') {
-      const { repository, secret } = req.body;
-         const { name, clone_url } = repository;
-         console.log(`Updating ${name} repository`);
-         const git = spawn('git', ['pull'], { cwd: `${REPO_PATH}/${name}` });
-         git.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-         });
-         git.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-         });
-         git.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-         });
-         res.sendStatus(200);
-   } else {
+   const { exec } = require('child_process');
+
+   const repoPath = process.cwd(); // get the current working directory as the repository path
+   const branch = 'main'; // replace with the branch you want to pull from
+
+   exec(`cd ${repoPath} && git pull origin ${branch}`, (error, stdout, stderr) => {
+      if (error) {
+         console.error(`Error: ${error.message}`);
+         return;
+      }
+      if (stderr) {
+         console.error(`stderr: ${stderr}`);
+         return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
       res.sendStatus(200);
-   }
+  
 });
 app.listen(80, '0.0.0.0')
 
