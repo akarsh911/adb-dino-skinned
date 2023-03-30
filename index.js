6f2b4631a0e40affe5caa796f9905b2b23e28c0d
akarsh <akarshagri121@gmail.com>
2023-03-30 17:36:21 +0530

    aks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 app.use(express.static('public'))
 app.use('/static', express.static('public'));
 app.get("/", function (req, res) {
-  res.sendStatus(200)
+   res.sendFile('index.html')
 })
 app.use(express.json());
 
