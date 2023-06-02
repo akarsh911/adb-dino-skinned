702fdb6a82d2b485d7faa690c2fa2cd85f90876d
akarsh <akarshagri121@gmail.com>
2023-06-03 01:17:15 +0530

    aks

--- middlewares/user.login.js
+++ middlewares/user.login.js
@@ -31,10 +31,12 @@ exports.verify= async(sid,uid)=>{
     if (await account.exists({ user_id: uid, session_id: sid, status:1})) {
         console.log("account found")
         var user_account = await user.findOne({ user_id: uid });
-        delete user_account.pwd_hash;
-        console.log(user_account)
+       
         if (user_account)
-            return user_account;
+        {
+            delete user_account.pwd_hash;
+             console.log(user_account)
+            return user_account;}
         else
             return false;
     }
