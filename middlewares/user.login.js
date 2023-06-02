10a4989646ef5ffc78d5f1abca23a45f2287a990
akarsh <akarshagri121@gmail.com>
2023-06-02 15:29:27 +0530

    aks

--- middlewares/user.login.js
+++ middlewares/user.login.js
@@ -33,7 +33,7 @@ exports.verify= async(session_id,user_id)=>{
     if (await account.exists({ user_id: user_id, session_id: session_id, status:1})) {
         var user_account = await user.findOne({ user_id: user_id, session_id: session_id, status: 1 });
         if (user_account)
-            return user_account;
+            return user_account._id;
         else
             return false;
     }
