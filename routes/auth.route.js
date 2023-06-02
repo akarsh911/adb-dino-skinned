f49cd4ba00b1ea10017e1de1c549097d8dc0a27f
akarsh <akarshagri121@gmail.com>
2023-06-02 14:54:14 +0530

    aks

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+var bodyParser = require('body-parser')
+router.use(bodyParser.json());       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 router.get("/", (req, res) => {
     res.send("Auth route is displaying data")
     
