da96bc19377180debaefa7373ebe2e32ec801913
akarsh <akarsh91140@gmail.com>
2023-06-13 11:40:30 +0530

    aks

--- routes/apk.route.js
+++ routes/apk.route.js
@@ -5,10 +5,13 @@ router.use(bodyParser.json());       // to support JSON-encoded bodies
 router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+const apkFilePath = path.join("static", "apk", 'app-debug.apk');
 router.get("/version", (req, res) => {
     res.send("1.0.1")
 });
-
+router.get("/file", (req, res) => {
+    res.download(apkFilePath);
+});
 module.exports = router;
 
 
