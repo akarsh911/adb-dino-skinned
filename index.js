6db29b7303cd7f2251ae05d5d78ab7cc312f9f1a
akarsh911 <akarsh91140@gmail.com>
2023-06-21 15:32:53 +0530

    aks

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.post("/", function (req, res) {
 })
 start_routes();
 app.post('/git-webhook', (req, res) => {
-   console.log("Git init");
+   console.log("Git init- "+ process.cwd());
    const { exec } = require('child_process');
-   exec('sudo git status', (error, stdout, stderr) => {
+   exec('git pull origin master', (error, stdout, stderr) => {
       if (error) {
          console.error(`Error: ${error.message}`);
          res.status(500).send('Git pull failed');
